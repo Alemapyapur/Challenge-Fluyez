7897dad029e625c61c3114b47565125156bfd077
Alemap Yapur <pamela.rupay31@gmail.com>
2022-02-19 19:35:16 -0500

    commit inicial

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,17 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import Sidebar from "./Sidebar"
+
+
+function Layout() {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Header/>
+      <Sidebar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Layout
