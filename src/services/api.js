9069174a2b64f8767ed0785b9aa417022efa02b2
Alemap Yapur <pamela.rupay31@gmail.com>
2022-02-22 01:44:43 -0500

    fixed: env

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const API = axios.create({
   // baseURL: "http://www.omdbapi.com/?i=tt3896198&apikey=3d19be93&type=movie"
-  // baseURL: process.env.REACT_APP_API,
-  baseURL: "http://www.omdbapi.com",
+  baseURL: process.env.REACT_APP_API,
+  // baseURL: "http://www.omdbapi.com",
 });
--- src/services/apiKey.js
+++ src/services/apiKey.js
@@ -1,2 +1,2 @@
-// export const APIKey = process.env.REACT_APP_API_TOKEN;
-export const APIKey = "3d19be93";
+export const APIKey = process.env.REACT_APP_API_TOKEN;
+// export const APIKey = "3d19be93";
